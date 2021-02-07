acaa29cbce2804293acc2cf9dddfdfab7599b54c
Muhammet Arif Sarikaya <muhammedarif82@gmail.com>
2021-02-08 01:26:54 +0300

    login screen created

--- lib/main.dart
+++ lib/main.dart
@@ -5,43 +5,17 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Login Screen',
+      home: MyHomePage(title: 'Login Screen'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -50,68 +24,333 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  TextEditingController _controller;
+  bool _isPasswordVisible = false;
+  bool isRemembered = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Colors.black,
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 75.0, bottom: 25.0),
+                    child: Text(
+                      "Login",
+                      style: TextStyle(color: Colors.white, fontSize: 40),
+                    ),
+                  ),
+                  buildGoogleButton(),
+                  buildFacebookButton(),
+                  buildDividerWithText(),
+                  buildUsernameInputColumn(),
+                  buildPasswordInputColumn(),
+                  buildRememberAndForgotPasswordRow(),
+                  buildLoginButton(),
+                  buildSignUpRow()
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+
+  Row buildSignUpRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Don't have an account?",
+          style: TextStyle(color: Color(0xFF595858)),
+        ),
+        TextButton(
+          onPressed: () => {},
+          child: Text(
+            'Sign Up',
+            style: TextStyle(color: Colors.blue),
+          ),
+        )
+      ],
+    );
+  }
+
+  SizedBox buildLoginButton() {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: RaisedButton(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+          color: Color(0xFF272727),
+          onPressed: () => {},
+          textColor: Colors.white,
+          padding: const EdgeInsets.all(0.0),
+          child: Container(
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(8.0)),
+              gradient: LinearGradient(
+                colors: <Color>[
+                  Color(0xFFF66053),
+                  Color(0xFFCE2E70),
+                ],
+              ),
+            ),
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Login', style: TextStyle(fontSize: 20)),
+              ],
+            ),
+          ),
+        ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    );
+  }
+
+  Row buildRememberAndForgotPasswordRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            Switch(
+              value: isRemembered,
+              onChanged: (value) {
+                setState(() {
+                  isRemembered = value;
+                });
+              },
+              activeTrackColor: Colors.green,
+              activeColor: Colors.greenAccent,
+              inactiveThumbColor: Colors.white,
+              inactiveTrackColor: Colors.blue,
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'Remember me',
+              style: TextStyle(color: Color(0xFF595858)),
             ),
           ],
         ),
+        FlatButton(
+          onPressed: () => {},
+          child: Text(
+            'Forgot Password?',
+            style: TextStyle(color: Color(0xFF595858)),
+          ),
+        )
+      ],
+    );
+  }
+
+  Padding buildUsernameInputColumn() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Text(
+                  "Username",
+                  style: TextStyle(color: Color(0xFF595858)),
+                ),
+              )
+            ],
+          ),
+          TextField(
+            controller: _controller,
+            style: TextStyle(color: Colors.white),
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.all(16.0),
+              fillColor: Color(0xFF272727),
+              filled: true,
+              focusedBorder: OutlineInputBorder(
+                borderRadius: new BorderRadius.circular(8.0),
+              ),
+              enabledBorder: UnderlineInputBorder(
+                borderRadius: new BorderRadius.circular(8.0),
+              ),
+            ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Padding buildPasswordInputColumn() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Text(
+                  "Password",
+                  style: TextStyle(color: Color(0xFF595858)),
+                ),
+              )
+            ],
+          ),
+          TextField(
+            controller: _controller,
+            style: TextStyle(color: Colors.white),
+            obscureText: !_isPasswordVisible,
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.all(16.0),
+              suffixIcon: _isPasswordVisible
+                  ? IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _isPasswordVisible = !_isPasswordVisible;
+                        });
+                      },
+                      icon: Icon(
+                        Icons.visibility_off,
+                        color: Color(0xFF595858),
+                      ),
+                    )
+                  : IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _isPasswordVisible = !_isPasswordVisible;
+                        });
+                      },
+                      icon: Icon(
+                        Icons.visibility,
+                        color: Color(0xFF595858),
+                      ),
+                    ),
+              fillColor: Color(0xFF272727),
+              filled: true,
+              focusedBorder: OutlineInputBorder(
+                borderRadius: new BorderRadius.circular(8.0),
+              ),
+              enabledBorder: UnderlineInputBorder(
+                borderRadius: new BorderRadius.circular(8.0),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Padding buildDividerWithText() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: Divider(
+            color: Color(0xFF595858),
+          )),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Text(
+              "Or",
+              style: TextStyle(color: Color(0xFF595858)),
+            ),
+          ),
+          Expanded(
+              child: Divider(
+            color: Color(0xFF595858),
+          )),
+        ],
+      ),
+    );
+  }
+
+  SizedBox buildGoogleButton() {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: RaisedButton(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+          color: Color(0xFF272727),
+          onPressed: () => {},
+          textColor: Colors.white,
+          padding: const EdgeInsets.all(0.0),
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 16.0),
+                  child: Image(
+                    image: AssetImage('assets/google.png'),
+                    width: 24.0,
+                    height: 24.0,
+                  ),
+                ),
+                const Text('Use Google Account',
+                    style: TextStyle(color: Color(0xFF595858))),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  SizedBox buildFacebookButton() {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: RaisedButton(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+          color: Color(0xFF272727),
+          onPressed: () => {},
+          textColor: Colors.white,
+          padding: const EdgeInsets.all(0.0),
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 16.0),
+                  child: Image(
+                    image: AssetImage('assets/facebook.png'),
+                    width: 24.0,
+                    height: 24.0,
+                  ),
+                ),
+                const Text('Use Facebook Account',
+                    style: TextStyle(color: Color(0xFF595858))),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
